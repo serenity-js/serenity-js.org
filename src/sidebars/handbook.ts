1701b7c0c647494d99a3fd1336d3b43ac3861f36
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2026-06-01 15:19:13 +0100

    docs: add troubleshooting guide to handbook

--- src/sidebars/handbook.ts
+++ src/sidebars/handbook.ts
@@ -24,6 +24,7 @@ const sidebars: SidebarsConfig = {
             items: [
                 'installation',
                 'architecture',
+                'troubleshooting',
             ]
         },
         {
